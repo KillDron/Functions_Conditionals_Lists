ceda3f97622b0a541072fa56681636f561c695e6
AlekseenkoYurii <90180586+AlekseenkoYurii@users.noreply.github.com>
2021-09-20 00:37:50 +0300

    Add files via upload
    Question 15

---
+++ Питання 15.py
@@ -0,0 +1,7 @@
+def is_sorted(lst):
+    if lst == sorted(lst):
+        return True
+    elif lst == sorted(lst, reverse=True):
+        return False
+print(is_sorted([1, 2, 3]))
+print(is_sorted([3, 2, 1]))
