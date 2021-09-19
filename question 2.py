a638c89dd3c12908c721911638aca75360360a8a
MaxKhamuliak <90180534+MaxKhamuliak@users.noreply.github.com>
2021-09-20 00:55:14 +0300

    Add files via upload

---
+++ question 2.py
@@ -0,0 +1,9 @@
+x = int(input('How many credits you take?'))
+if x <= 23 :
+    print('you are freshman')
+if 23 < x < 54 :
+    print('you are sophomore')
+if 53 < x < 84 :
+    print('you are junior')
+if x >= 84 :
+    print('you are senior')
