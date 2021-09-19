6dea5919a27f88b251605ffdf3395cdccf10767b
MaxKhamuliak <90180534+MaxKhamuliak@users.noreply.github.com>
2021-09-20 00:57:36 +0300

    Add files via upload

---
+++ question 14.py
@@ -0,0 +1,13 @@
+def sumDigits(n):
+ 
+    sum = 0
+    while (n != 0):
+ 
+        sum = sum + int(n % 10)
+        n = int(n/10)
+ 
+    return sum
+ 
+ 
+n = int(input("Write your number"))
+print(sumDigits(n))
