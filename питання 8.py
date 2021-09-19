dae0ea8ed038262123d36545ce5fddaab00b2c73
SophiaOrshulyak <90265478+SophiaOrshulyak@users.noreply.github.com>
2021-09-20 01:31:56 +0300

    Add files via upload

---
+++ питання 8.py
@@ -0,0 +1,10 @@
+def mysort(mylist=[], numflag=False):
+    L = []
+    if numflag == False:
+        for i in mylist:
+            L.append(str(i))
+        print(sorted(L))
+    elif numflag == True:
+        for i in mylist:
+            L.append(int(i))
+        print(sorted(L))
