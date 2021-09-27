68345172c65ffba9b13dfba8bc45f43b6716ec6a
KillDron <90138629+KillDron@users.noreply.github.com>
2021-09-27 22:32:08 +0300

    Update Питання 9.py

--- Питання 9.py
+++ Питання 9.py
@@ -1,3 +1,5 @@
-inp = input('Введіть щось: ')
-def true_or_false(inp):
-    return inp
+Щось = input('Введіть щось: ')
+def T_or_F(x):
+    return False if x == "" or x == 0 else True
+
+print(T_or_F(Щось))
