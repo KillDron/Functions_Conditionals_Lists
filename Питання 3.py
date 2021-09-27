7adaf1bf1003e1fab815e2ef3a9d54fad66596d0
SophiaOrshulyak <90265478+SophiaOrshulyak@users.noreply.github.com>
2021-09-27 21:49:05 +0300

    Update Питання 3.py

--- Питання 3.py
+++ Питання 3.py
@@ -1,11 +1,10 @@
 import random
 A = random.randrange(1,11)
-F = "Не праві"
+F = "не праві"
 e = 0
-print(A)
-while F == "Не праві":
+while F == "не праві":
     if A == int(input("Ваше число:")):
-        F = "Правий"
+        F = "праві"
         e += 1
         print("Цього разу ви були", F)
         print("Кількість спроб",e)
