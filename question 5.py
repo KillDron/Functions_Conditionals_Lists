166158e018ec7e3237cf9804967971b7fcc6e47d
KillDron <90138629+KillDron@users.noreply.github.com>
2021-09-27 22:05:45 +0300

    Update question 5.py
    Змінив код щоб задовільняв умову задачі

--- question 5.py
+++ question 5.py
@@ -1,29 +1,35 @@
 import random
-
-while True:
+n = 3
+UW = 0
+CW = 0
+while n != 0:
     user_action = input("Enter a choice (rock, paper, scissors): ")
     possible_actions = ["rock", "paper", "scissors"]
     computer_action = random.choice(possible_actions)
-    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
-
+    print(f"You chose {user_action}, computer chose {computer_action}")
     if user_action == computer_action:
-        print(f"Both players selected {user_action}. It's a tie!")
+        UW += 1
+        CW += 1
     elif user_action == "rock":
         if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
+            UW += 1
         else:
-            print("Paper covers rock! You lose.")
+            CW += 1
     elif user_action == "paper":
         if computer_action == "rock":
-            print("Paper covers rock! You win!")
+            UW += 1
         else:
-            print("Scissors cuts paper! You lose.")
+            CW += 1
     elif user_action == "scissors":
         if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
+            UW += 1
         else:
-            print("Rock smashes scissors! You lose.")
+            CW += 1
+    n -= 1
 
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
-        break
+if UW > CW:
+    print("User Win")
+elif UW < CW:
+    print("Computer Win")
+else:
+    print("Draw")
